89356c420524c196ca438e91eebd9aae58e0f6a2
Nayanika <nayanika.0208@gmail.com>
2020-05-24 06:16:57 +0530

    adding sign-in sign-up page with form input component and reusable button component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
+import Directory from '../../components/directory/directory.component';
+
 import './homepage.styles.scss';
-import  Directory   from '../../components/directory/directory.component';
 
 const HomePage = () => (
   <div className='homepage'>
-    <h1>Welcome to my Homepage</h1>
-        <Directory />
+    <Directory />
   </div>
 );
 
